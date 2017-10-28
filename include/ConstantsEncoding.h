8c3ece44fbcfa0ea3fc7686b6aeb743fe3443ba6
nidhidhamnani <nidhidhamnani@gmail.com>
2017-10-28 19:48:39 +0530

    Added files for encode and decode

--- include/ConstantsEncoding.h
+++ include/ConstantsEncoding.h
@@ -8,11 +8,16 @@
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Analysis/ScalarEvolution.h"
 #include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/GlobalVariable.h"
+
 using namespace llvm;
 
 
 class ConstantsEncodingUtils {
 public:
+	static int encode(GlobalVariable*);
+
+	static void decode(GlobalVariable*, int);
 };
 
 class ConstantsEncoding : public FunctionPass {
